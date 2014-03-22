fd608a9df796f9b56b6742120058b7dbfb714645
Ricardo Gonzalez <rickgc@gmail.com>
2014-03-22 00:01:49 +0000

    js optim

---
+++ js/main.js
@@ -0,0 +1,106 @@
+jQuery(function() {
+
+  jQuery.fn.n33_formerize=function(){var _fakes=new Array(),_form = jQuery(this);_form.find('input[type=text],textarea').each(function() { var e = jQuery(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = jQuery(this); var x = jQuery(jQuery('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = jQuery(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = jQuery(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { jQuery(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = jQuery(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); jQuery(this).find('select').val(jQuery('option:first').val()); jQuery(this).find('input,textarea').each(function() { var e = jQuery(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
+  jQuery.browser={};(function(){jQuery.browser.msie=false;jQuery.browser.version=0;if(navigator.userAgent.match(/MSIE ([0-9]+)\./)){jQuery.browser.msie=true;jQuery.browser.version=RegExp.$1;}})();
+
+  var _bh = jQuery('body, html'),
+    _window = jQuery(window),
+    _nav = jQuery('#nav');
+
+  // Forms
+    if (jQuery.browser.msie && jQuery.browser.version <= 9)
+      jQuery('form').n33_formerize();
+
+    // jQuery('form .form-button-submit').click(function(e) { e.preventDefault(); jQuery(this).closest('form').submit(); });
+    jQuery('form .form-button-reset').click(function(e) { e.preventDefault(); jQuery(this).closest('form')[0].reset(); });
+
+  // Links
+    jQuery('a').click(function(e) {
+      var t = jQuery(this), h = t.attr('href'), article;
+
+      if (h.charAt(0) == '#' && h.length > 1 && (article = jQuery('article#' + h.substring(1))).length > 0)
+      {
+        var pos = Math.max(article.parent().offset().top - _nav.height() + 15, 0);
+        e.preventDefault();
+        _bh.animate({ scrollTop: pos }, 'slow', 'swing');
+      }
+    });
+
+    jQuery('.wrapper').css('min-height',$(window).height()+'px');
+
+
+  $("#submit").click(function() {
+    // validate and process form here
+
+    var errorflag = false;
+    var name = $("input#name");
+    if (name.val() == "") {
+      name.focus();
+      errorflag = true;
+    }
+    var email = $("input#email");
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (email.val() == "" || !re.test(email.val())) {
+      email.focus();
+      errorflag = true;
+    }
+    var subject = $("input#subject");
+    if (subject.val() == "") {
+      subject.focus();
+      errorflag = true;
+    }
+    var message = $("input#message");
+    if (message.val() == "") {
+      message.focus();
+      errorflag = true;
+    }
+
+    if(errorflag) {
+      return false;
+    }
+
+
+    var dataString = 'name='+ name.val() + '&email=' + email.val() + '&subject=' + subject.val() + '&message=' + message.val();
+
+    $.ajax({
+      type: "POST",
+      url: "process.php",
+      data: dataString,
+      success: function() {
+      // console.log('hola');
+
+      }
+    });
+    return false;
+
+
+  });
+
+
+
+  /* Docs nav highlighting */
+  $('article.panel')
+    .waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'down');
+      $(this).toggleClass('active', direction === 'down');
+    }, {
+      offset: '100%'
+    })
+    .waypoint(function(direction) {
+      var $links = $('a[href="#' + this.id + '"]');
+      $links.toggleClass('active', direction === 'up');
+      $(this).toggleClass('active', direction === 'up');
+    }, {
+      offset: function() {
+        return -$(this).height();
+      }
+    });
+
+    $("img").lazyload({
+        effect : "fadeIn",
+        threshold : 100
+    });
+
+
+});
